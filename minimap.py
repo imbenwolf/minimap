20a03d8de88c21b7f84c03454c367e8fffadbe19
Ben Wolf <benjamineliaswolf@gmail.com>
2020-09-21 10:10:53 +0200

    Add method to read fingerprint from socket and add it as optional argument for tcp_connect_scan

--- minimap.py
+++ minimap.py
@@ -4,6 +4,6 @@ import tcp
 
 
 def main():
-    for res in tcp.tcp_connect_scan("access-hat.ch", ['https', 444, 666]):
+    for res in tcp.tcp_connect_scan("192.168.220.211", ['ssh', 444, 666], True):
         print(res)
     return 0
--- tcp.py
+++ tcp.py
@@ -6,11 +6,29 @@ SOCKET_FIREWALLED = 2
 SOCKET_NO_SUCH_SERVICE = 3
 
 
-def tcp_connect_port(host, port, fingerprint=False):
+def read_fingerprint_from_socket(socket):
+    try:
+        data = bytearray()
+        while True:
+            chunk = socket.recv(4096)
+            if not chunk:
+                break
+            data.extend(chunk)
+    except socket.timeout:
+        pass
+    finally:
+        return data.decode('utf-8').strip() 
+
+
+def tcp_connect_port(host, port, fingerprint):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(2)  # 2 seconds
         s.connect((host, port))
+
+        if fingerprint:
+            return (SOCKET_OPEN, read_fingerprint_from_socket(s))
+
         return SOCKET_OPEN
     except ConnectionRefusedError:
         return SOCKET_CLOSED
@@ -18,9 +36,9 @@ def tcp_connect_port(host, port, fingerprint=False):
         return SOCKET_FIREWALLED
 
 
-def tcp_connect_scan(host, ports):
+def tcp_connect_scan(host, ports, fingerprint=False):
     for port in ports:
-        
+
         # Get port by service name if port is not a number
         if not str(port).isdigit():
             try:
@@ -29,4 +47,8 @@ def tcp_connect_scan(host, ports):
                 yield (port, SOCKET_NO_SUCH_SERVICE)
                 continue
 
-        yield (port, tcp_connect_port(host, port))
+        result = tcp_connect_port(host, port, fingerprint)
+        if isinstance(result, tuple):
+            yield (port, *result)
+        else:
+            yield (port, result)
